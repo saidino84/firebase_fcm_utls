dadae63ab61ddd36498854bf97238f6b18e1d600
saidino84 <tylorguy2018@gmail.com>
2021-11-07 15:10:51 +0200

    modificacoes bacanas

--- post.py
+++ post.py
@@ -12,7 +12,7 @@ headers={
     'Authorization':auth
 }
 
-# USERS
+# USERS [esses ids obtive nos telefones da minha mulher e da minha mae]
 wife='ePbRZsCgTDuHwVhHbTsEGT:APA91bGJY2xrgKlMnD7Inf95jjPBc404sTK2nfh4fqb_HEWzHvgOCHrzYV-K_Hu68X0941kx_1coFUQcl9xfRaShZhmha6lMixGnXXFOjkWN8NG8L6z6msrj2I5f8Kk1m7HOcE0lQUNV'
 mine="dA8SJnyqSGGF5s6mWJ_wp2:APA91bHInq0DgMVL5usx_zCiJXwapXLioqOXnAoGd9Df3WoFyAQEB0bfFqWX-KiQ2FmXbmMhIAjAXuUkTEPZxe8zPwfJPrRiWG55CV0BEKnG_urbNCfAmCf_y8RA-p4NT56hIWudOeRU"
 # [body]
@@ -22,12 +22,12 @@ image_url=input('image link')
 if(len(image_url) <10):
     image_url='https://st.depositphotos.com/1063437/2337/i/950/depositphotos_23373292-stock-photo-groceries-in-wicker-basket-including.jpg'
 body={
-    "to":wife,
+    "to":'/topics/all',
     "notification":{
         "body":message,
         "title":title,
         "image":image_url
-        
+
     },
     "data":{
         "body":"Body :Data",
