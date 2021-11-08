2b307c60f9c50726cb81e760b13035aea52489b1
saidino84 <tylorguy2018@gmail.com>
2021-11-08 19:38:10 +0200

    ading to new  brunch

--- remote_api.py
+++ remote_api.py
@@ -2,6 +2,7 @@ import json
 import requests as rq
 
 api_uri='http://localhost:5000/spar/postproduct/'
+api_uri_main='http://saidinosecondapp.herokuapp.com/spar/postproduct/'
 auth='key=AAAAT7I801w:APA91bFyk0q5leLPRR8YtKy3Q1s1wA38IcLdhZbkXF3ysCY7HdwOZ2IJEMQoaO8t5zKTwE_A8hockdmrL1FgkKkbqfK6FX4eCP5R7o1XLliD-e3OYFY1aruXPxac9rc_x6Dr6ly17nER'
 
 fcm_uri='https://fcm.googleapis.com/fcm/send'
