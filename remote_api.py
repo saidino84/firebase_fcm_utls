fb78103ef96b7e9d489a0de708ab4a1af1df2798
saidino84 <tylorguy2018@gmail.com>
2021-11-12 14:34:23 +0200

    post.py working sucessfuly with image

--- remote_api.py
+++ remote_api.py
@@ -1,17 +1,20 @@
 import json
 import requests as rq
 
-api_uri='http://saidinosecondapp.herokuapp.com/spar/postproduct/'
+# api_uri='http://saidinosecondapp.herokuapp.com/spar/postproduct/'
+api_uri='http://localhost:5000/spar/postproduct/'
 
 def send_notification(message,title,image,to='/topics/ADMIN'):
     data={
         'to':'/topics/ADMIN',
         'body': message,
-        'title':title',
+        'title':title,
         'image': image,
                 }
     try:
-        rq.post(api_uri,data=json.dumps(data))
-
-
-print(resp.json)
+        resp=rq.post(api_uri,json=json.dumps(data))
+        print(resp.text)
+    except  Exception as e:
+        print(e)
+image='https://vipspar.com/wp-content/uploads/2020/07/38145-480x480.jpg'
+send_notification(message="The message",title="title",image=image)
